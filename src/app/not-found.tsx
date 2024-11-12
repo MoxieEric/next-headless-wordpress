ea90f4405767d77eb36e8b71506b35bc8830eb6c
Eric Nowels <ericnowels@gmail.com>
2024-11-12 14:06:13 -0700

    add wordpress theme and basic config core files, plugins and content is ignored

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import { print } from "graphql/language/printer";
+import { print } from 'graphql/language/printer'
+import type { Metadata } from 'next'
 
-import { setSeoData } from "@/utils/seoData";
+import { setSeoData } from '@/utils/seoData'
 
-import { fetchGraphQL } from "@/utils/fetchGraphQL";
-import { ContentNode, Page } from "@/gql/graphql";
-import { PageQuery } from "@/components/Templates/Page/PageQuery";
-import { SeoQuery } from "@/queries/general/SeoQuery";
+import { PageQuery } from '@/components/Templates/Page/PageQuery'
+import { ContentNode, Page } from '@/gql/graphql'
+import { SeoQuery } from '@/queries/general/SeoQuery'
+import { fetchGraphQL } from '@/utils/fetchGraphQL'
 
-const notFoundPageWordPressId = 501;
+const notFoundPageWordPressId = 501
 
 export async function generateMetadata(): Promise<Metadata> {
-  const { contentNode } = await fetchGraphQL<{ contentNode: ContentNode }>(
-    print(SeoQuery),
-    { slug: notFoundPageWordPressId, idType: "DATABASE_ID" },
-  );
+	const { contentNode } = await fetchGraphQL<{ contentNode: ContentNode }>(
+		print(SeoQuery),
+		{ slug: notFoundPageWordPressId, idType: 'DATABASE_ID' }
+	)
 
-  const metadata = setSeoData({ seo: contentNode.seo });
+	const metadata = setSeoData({ seo: contentNode.seo })
 
-  return {
-    ...metadata,
-    alternates: {
-      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/404-not-found/`,
-    },
-  } as Metadata;
+	return {
+		...metadata,
+		alternates: {
+			canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/404-not-found/`,
+		},
+	} as Metadata
 }
 
 export default async function NotFound() {
-  const { page } = await fetchGraphQL<{ page: Page }>(print(PageQuery), {
-    id: notFoundPageWordPressId,
-  });
+	const { page } = await fetchGraphQL<{ page: Page }>(print(PageQuery), {
+		id: notFoundPageWordPressId,
+	})
 
-  return <div dangerouslySetInnerHTML={{ __html: page.content || " " }} />;
+	return <div dangerouslySetInnerHTML={{ __html: page?.content || ' ' }} />
 }
