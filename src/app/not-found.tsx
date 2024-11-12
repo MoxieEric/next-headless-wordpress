9cfc4f6533dcd9ad16947c1ced39996ef8f0701e
Eric Nowels <ericnowels@gmail.com>
2024-11-12 16:44:53 -0700

    not found ID

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ import { ContentNode, Page } from '@/gql/graphql'
 import { SeoQuery } from '@/queries/general/SeoQuery'
 import { fetchGraphQL } from '@/utils/fetchGraphQL'
 
-const notFoundPageWordPressId = process.env.NEXT_PUBLIC_WORDPRESS_404_ID || 6
+const notFoundPageWordPressId = process.env.NEXT_PUBLIC_WORDPRESS_404_ID || 8
 
 export async function generateMetadata(): Promise<Metadata> {
 	const { contentNode } = await fetchGraphQL<{ contentNode: ContentNode }>(
